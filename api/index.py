6d216df791a53cfd47acc6e5b81af4a159858e87
ZubairBABA <info@food-pedia.ch>
2026-02-19 14:45:39 +0100

    fix(deploy): Add sys.path hack to api/index.py for Vercel

--- api/index.py
+++ api/index.py
@@ -1,4 +1,11 @@
+import os
 import sys
+
+# Add project root to sys.path for Vercel
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 import traceback
 from fastapi import FastAPI, Response
 
